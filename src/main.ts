e7e63b9cbc2d6013e3e14e5bc467ed5811a95fba
Sam Genduso <sam.genduso@gmail.com>
2018-12-27 10:03:02 -0500

    the thing works with vue and ionic and can run on android device

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Ionic from '@ionic/vue';
+
+Vue.use(Ionic);
 
 Vue.config.productionTip = false;
 
